14656ad7ea58e19d3c4bb4418d2da3c5b0f08209
Juan Chavez <juan@Juans-Air.home>
2022-04-11 13:12:14 -0700

    added SPA attributes to components

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -26,7 +26,7 @@ function ContactForm() {
             }
         }
         console.log('errorMessage', errorMessage);
-        
+
         if (!errorMessage) {
             setFormState({ ...formState, [e.target.name]: e.target.value });
         }
@@ -44,16 +44,22 @@ function ContactForm() {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onChange={handleChange}/>
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
                 </div>
+                {/* if errorMessage true, render the following */}
+                {errorMessage && (
+                    <div>
+                    <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
